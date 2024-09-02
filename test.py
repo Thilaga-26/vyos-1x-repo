ca63805258f9a382b24666d5bb94cff52f587f9d
Thilaga-26 <112231561+Thilaga-26@users.noreply.github.com>
2024-09-02 13:33:47 +0530

    vyos-1x

---
+++ test.py
@@ -0,0 +1,16 @@
+from os import path 
+import re
+import date
+import datetime
+import request
+import csv
+import json
+
+def hello_world():
+  print("Hello, world!")
+  print("Test Case1") 
+  print("Another line")# No Space check 
+  print("Another one more lin1234e")
+  print("Test Indentation3") 
+  
+hello_world()
---
+++ testcase.py
@@ -0,0 +1,14 @@
+# example_with_imports.py
+
+import os
+import sys
+import datetime
+
+def main():
+    print("Hello, World!")
+    print(f"Current working directory: {os.getcwd()}")
+    print(f"Python version: {sys.version}")
+    print(f"Test Case for Linting with Ruff and Darker")# Space Testing
+
+if __name__ == "__main__":
+    main()
